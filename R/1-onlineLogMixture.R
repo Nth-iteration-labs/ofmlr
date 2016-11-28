c47e04b8eee1546294dd04235ea1749aeac057bf
Maurits <maurits@mauritskaptein.com>
2016-11-28 11:14:13 +0100

    Changes in default learn rate 2

--- R/1-onlineLogMixture.R
+++ R/1-onlineLogMixture.R
@@ -154,7 +154,7 @@ setMethod(
 		
 		# Learn rate from Stochastic Variational inference:
 		if(lambda<=0){
-			lambda <- (object@params$n+100)^-.5
+			lambda <- (object@params$n)^-.5
 		}
 		
 		# M Step:

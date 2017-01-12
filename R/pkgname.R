3fc571f6977462f1dd5e1d37ad1a67b16476852c
Maurits <maurits@mauritskaptein.com>
2017-01-12 14:11:55 +0100

    Documentation

--- R/pkgname.R
+++ R/pkgname.R
@@ -1,22 +1,11 @@
-#' Fit online finite mixtures of logistic regression models
+#' foo: A package for computating the notorious bar statistic.
 #'
-#’ The ofmlr package allows one to fit, online (or in a data stream) multiple finite 
-#' mixtures of logistic regression models in parallel. The main workhorse \code{online_log_mixture()} 
-#' and its utility functions (such as \code{add_observation()} can be used to fit a model. 
-#' The \code{multi_online_log_mixture} object allows one to fit multiple models in paralel
-#' and compare their outcomes.
-#'
-#' @seealso Please see the more extensive documentation and examples under \code{\link{online_log_mixture()}}
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
 #' 
-#' @examples
-#' M1 <- online_log_mixture(3,3, trace=1)
-#' for(i in 1:10000){ 	
-#' 	X <- runif(3,-2,2)
-#' 	y <- rbinom(1, 1, inv_logit(c(0,-2,2)%*%X))
-#' 	M1 <- add_observation(M2, y, X, 0)
-#' }
-#' plot(M1, params=TRUE)
+#' @section Foo functions:
+#' The foo functions ...
 #'
 #' @docType package
-#' @name ofmlr
+#' @name foo
 NULL

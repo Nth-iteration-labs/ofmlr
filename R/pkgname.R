b59de37532e23062248e04b442d90dd40a14625e
Maurits <maurits@mauritskaptein.com>
2017-01-12 09:44:09 +0100

    Documentation update

--- R/pkgname.R
+++ R/pkgname.R
@@ -1,10 +1,11 @@
 #' Fit online finite mixtures of logistic regression models
 #'
+#'
 #’ The package allows one to fit, online (or in a data stream) an multiple finite 
 #' mixtures of logistic regression models. The main workhorse \code{online_log_mixture()} 
 #' and its utility functions (such as \code{add_observation()} can be used to fit a model. 
 #' The \code{multi_online_log_mixture} object allows one to fit multiple models in paralel
-#' and compare their outcomes.
+#' and compare their outcomes. HAHA.
 #' @examples
 #' M1 <- online_log_mixture(3,3, trace=1)
 #' for(i in 1:10000){ 	
